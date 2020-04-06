9f6ae1856eacc16b3b3b3cb99b61c7a1b3a4947e
Sohail Haider <sohailh343@gmail.com>
2020-04-06 10:22:05 +0000

    properties resolver added

--- server/src/resolvers/index.js
+++ server/src/resolvers/index.js
@@ -3,5 +3,31 @@ const Mutation = require('./Mutation.js')
 
 module.exports = {
     Query,
-    Mutation
+    Mutation,
+    User: {
+        visits: async ({id}, args, context) => {
+            return context.prisma.user({id}).visits();
+        }
+    },
+    Visit: {
+        location: async({id}, args, context) => {
+            return context.prisma.visit({id}).location();
+        },
+        duration: async({id}, args, context) => {
+            return context.prisma.visit({id}).duration();
+        },
+        user: async({id}, args, context) => {
+            return context.prisma.visit({id}).user();
+        },
+    },
+    Location: {
+        visit: async({id}, args, context) => {
+            return context.prisma.location({id}).visit();
+        },
+    },
+    Duration: {
+        visit: async({id}, args, context) => {
+            return context.prisma.duration({id}).visit();
+        }
+    }
 }
