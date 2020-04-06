e6846a5b9b7020636bdd3c2d5a27b22f08c86323
Sohail Haider <sohailh343@gmail.com>
2020-04-06 11:45:59 +0000

    getUserVisits and getAllVisits added

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,8 +1,18 @@
+
+const { APP_SECRET, getUserId } = require('../../utils.js');
+
 module.exports = {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
     emailExists: async (parent, args, context) => {
         return context.prisma.$exists.user({
           email: args.email,
         });
+    },
+    getUserVisits: async (parent, args, context) => {
+        const userId = getUserId(context); 
+        return context.prisma.user({id: userId}).visits();
+    },
+    getAllVisits: async (parent, args, context) => {
+        return context.prisma.visits();
     }
 }
